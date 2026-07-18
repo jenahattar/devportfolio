85e3a9fc7667be8ef8a6eadfa9581ed0ba1ef91d
“JenaHattar” <JenaHattar2009@gmail.com>
2026-07-18 10:37:47 -0700

    adding files

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jenahattar.github.io"
+  base: "/devportfolio/"
   vite: {
     plugins: [tailwindcss()],
   },
